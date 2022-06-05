12bc1221b5bcb187f0a46b9414253b5e4f40dd70
ludwig <murimiludwig@gmail.com>
2022-06-06 00:09:44 +0300

    creation of the home vie and thhe post model

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'insta/home.html', context)
