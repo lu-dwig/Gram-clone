60a660a7aacb3efe41c010081cb82cb924992aa3
ludwig <murimiludwig@gmail.com>
2022-06-06 01:15:44 +0300

    creation of the urls

--- insta/urls.py
+++ insta/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     
-    path('', views.home, name='insta-home'),
-    path('about/', views.about, name='insta-about'),   
+    path('', views.home, name='home'),
+    # path('about/', views.about, name='about'),
+    # path('profile/<username>/', views.profile, name='profile'),
+    # path('single_post', views.single_post, name='single_post'),
+    
 ]
