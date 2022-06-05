eff85eac1f3e7897c0cbb8937564a9ac7c85dcad
ludwig <murimiludwig@gmail.com>
2022-06-06 00:25:32 +0300

    creation of the superuser

--- instagram/urls.py
+++ instagram/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.urls import path
-from django.contrib import admin
+from django.contrib import admin, include
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+     path('', include('insta.urls')),
 ]
