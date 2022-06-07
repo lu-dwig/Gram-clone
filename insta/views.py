3350d75f2199be6b258da09b692b9c6134114d85
ludwig <murimiludwig@gmail.com>
2022-06-07 12:21:08 +0300

    creation of the update post functionality

--- insta/views.py
+++ insta/views.py
@@ -1,7 +1,9 @@
-from email.mime import image
-from re import template
+# from email.mime import image
+# from re import template
+
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 # Create your views here.
 
@@ -22,7 +24,15 @@ class PostDetailView(DetailView):
     model= Post
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin ,CreateView):
+    model= Post
+    fields = ['image', 'title',  'content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+  
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin ,UpdateView):
     model= Post
     fields = ['image', 'title',  'content']
     
@@ -30,6 +40,23 @@ class PostCreateView(CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
     
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeteteView(LoginRequiredMixin, UserPassesTestMixin ,DeleteView):
+    model= Post
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+ 
 def about(request):
     
     
