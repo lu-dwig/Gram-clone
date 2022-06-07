f4a0b42f3281097d889e74f73d2bbfcc341ed679
ludwig <murimiludwig@gmail.com>
2022-06-07 11:35:59 +0300

    creation of the post creation form

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,4 @@
+from email.mime import image
 from re import template
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
@@ -21,8 +22,14 @@ class PostDetailView(DetailView):
     model= Post
 
 
-class PostCreateView(DetailView):
+class PostCreateView(CreateView):
     model= Post
+    fields = ['image', 'title',  'content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
 def about(request):
     
     
