9bb854e795ae0182b72568b268a57c540fef95f0
ludwig <murimiludwig@gmail.com>
2022-06-08 08:49:12 +0300

    modification of the app

--- insta/urls.py
+++ insta/urls.py
@@ -4,7 +4,7 @@ from .views import PostListView, PostDetailView,PostCreateView, PostUpdateView, 
 
 urlpatterns = [
     
-    path('home/', PostListView.as_view(), name='home'),
+    path('', PostListView.as_view(), name='home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
