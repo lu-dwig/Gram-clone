7e07e1e33ecaf762b60cd18b349f5b33ccfb87fd
ludwig <murimiludwig@gmail.com>
2022-06-09 11:31:05 +0300

    migration modifications

--- instagram/settings.py
+++ instagram/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 from pathlib import Path
+import django_on_heroku 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,9 +138,14 @@ STATICFILES_DIR = [
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_on_heroku.settings(locals())
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
 LOGIN_REDIRECT_URL = '/home'
 LOGIN_URL = 'login'
+
+
