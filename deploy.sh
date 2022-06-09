f56dd466d5e387afd6f1817c6aea9f24eff9758d
ludwig <murimiludwig@gmail.com>
2022-06-09 16:31:27 +0300

    heroku deployment

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,11 @@ git add .
 git commit -m "heroku deployment"
 git push heroku master
 
-heroku config:set ALLOWED_HOSTS=agile-gorge-70085.herokuapp.com
-heroku config:set DEBUG=False
-heroku config:set DISABLE_COLLECTSTATIC=1
-heroku config:set MODE=prod
-heroku config:set SECRET_KEY='0k+t7_=#aouv$ars(-%ja&_li=^@0g#ci()k7d0=!)a7)ls88h'
+# heroku config:set ALLOWED_HOSTS=agile-gorge-70085.herokuapp.com
+# heroku config:set DEBUG=False
+# heroku config:set DISABLE_COLLECTSTATIC=1
+# heroku config:set MODE=prod
+# heroku config:set SECRET_KEY='0k+t7_=#aouv$ars(-%ja&_li=^@0g#ci()k7d0=!)a7)ls88h'
 
 
 heroku run python manage.py makemigrations
