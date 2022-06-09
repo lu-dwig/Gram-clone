e0189f983da7220b608684025c942617464c9b43
ludwig <murimiludwig@gmail.com>
2022-06-09 11:00:17 +0300

    modification of the sql migrations

--- users/views.py
+++ users/views.py
@@ -9,7 +9,7 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save(force_insert=False) 
+            form.save() 
             username = form.cleaned_data.get('username')
             messages.success(request,f'Your Account has been  Created!You are now able to log in')
             return redirect('login')
