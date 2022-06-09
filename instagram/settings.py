08170517a55b137f26e30e9e439220a69d94a89e
ludwig <murimiludwig@gmail.com>
2022-06-09 16:44:46 +0300

    heroku deployment

--- instagram/settings.py
+++ instagram/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
